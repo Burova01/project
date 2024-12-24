2070f1b030d24f1db61eac10699b86e431770303
Burova01 <ksyu.burova.0606@bk.ru>
2024-12-24 14:52:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 fetch('data.csv')
   .then(response => response.text())
   .then(data => {
-    const appointments = Papa.parse(data, {header: true}).data; // Используем библиотеку Papa Parse для парсинга CSV
+    const appointments = Papa.parse(data, {header: true}).data; // РСЃРїРѕР»СЊР·СѓРµРј Р±РёР±Р»РёРѕС‚РµРєСѓ Papa Parse РґР»СЏ РїР°СЂСЃРёРЅРіР° CSV
     const appointmentsContainer = document.getElementById('appointments');
     appointments.forEach(appointment => {
       const appointmentDiv = document.createElement('div');
       appointmentDiv.classList.add('appointment');
       appointmentDiv.innerHTML = `
         <h3>${appointment.patient_name}</h3>
-        <p>Врач: ${appointment.doctor_name}</p>
-        <p>Дата: ${appointment.appointment_date}</p>
-        <p>Время: ${appointment.appointment_time}</p>
-        <p>Статус: ${appointment.status}</p>
+        <p>Р’СЂР°С‡: ${appointment.doctor_name}</p>
+        <p>Р”Р°С‚Р°: ${appointment.appointment_date}</p>
+        <p>Р’СЂРµРјСЏ: ${appointment.appointment_time}</p>
+        <p>РЎС‚Р°С‚СѓСЃ: ${appointment.status}</p>
       `;
       appointmentsContainer.appendChild(appointmentDiv);
     });
   });
-
-
-
-
-
